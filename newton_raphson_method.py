0d417107bc5ac517a4eea4274e4bc56cee079270
PRO <39678394+Protap-mistry@users.noreply.github.com>
2020-01-25 21:41:53 +0600

    Newton_Raphson_method

---
+++ newton_raphson_method.py
@@ -0,0 +1,26 @@
+# The function is x^3 - x^2 + 2
+def func(x):
+    return x * x * x - x * x + 2
+
+# Derivative of the above function
+# which is 3*x^x - 2*x
+def derivFunc(x):
+    return 3 * x * x - 2 * x
+
+
+# Function to find the root
+def newtonRaphson(x):
+    h = func(x) / derivFunc(x)
+    while abs(h) >= 0.0001:
+        h = func(x) / derivFunc(x)
+
+        # Formula: x(i+1) = x(i) - f(x) / f'(x)
+        x = x - h
+
+    print("The value of the root is : ",
+          "%.4f" % x)
+
+
+# Driver program to test above
+x0 = 5  # Initial values assumed
+newtonRaphson(x0)
